604fda8a38ed97da3a67ab0d0c352c411499bc82
y617012389 <y617012389@gmail.com>
2024-11-15 03:09:37 +0800

    Site updated: 2024-11-15 03:09:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/08/Openwrt防检测—PC/","2024/11/14/学习通刷课/","2024/11/12/ios17.0+侧载方案—NB全能助手/"];function toRandomPost(){
+var posts=["2024/11/08/01/","2024/11/14/03/","2024/11/12/02/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
