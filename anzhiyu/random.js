620bb915e6ae00f8d9956b8f73b5feb62d784064
y617012389 <y617012389@gmail.com>
2024-11-18 13:58:50 +0800

    Site updated: 2024-11-18 13:58:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/08/01/","2024/11/14/03/","2024/11/12/02/"];function toRandomPost(){
+var posts=["2024/11/18/openwrt2/","2024/11/14/Tampermonkey/","2024/11/12/02/","2024/11/08/openwrt1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
